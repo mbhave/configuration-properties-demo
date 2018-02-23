48e93c98e50346f010e864d99beaec531929c656
Madhura Bhave <mbhave@pivotal.io>
2018-02-23 07:17:43 -0500

    Demo reading in uniform format

--- 2.0/src/main/java/com/example/demo/Application.java
+++ 2.0/src/main/java/com/example/demo/Application.java
@@ -1,8 +1,11 @@
 package com.example.demo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.env.Environment;
 
 @SpringBootApplication
 @EnableConfigurationProperties(HelloProperties.class)
@@ -10,5 +13,15 @@ public class Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
+	}
+
+	@Bean
+	public CommandLineRunner test(Environment environment) {
+		return new CommandLineRunner() {
+			@Override
+			public void run(String... args) throws Exception {
+				System.out.println(environment.getProperty("health.show-details"));
+			}
+		};
 	}
 }
